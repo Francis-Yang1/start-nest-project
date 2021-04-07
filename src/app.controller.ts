c361fa598c779d33639e4275cd6f26f30f589ff8
杨莫莫桑桑 <1285511723@qq.com>
2021-04-07 11:43:03 +0800

    Revert "feat: 添加查询接口参数案例,用@Query()"
    This reverts commit 0998ea1ba769488bc27229a0e283b3105255784c.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
-
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,10 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  //post用@Body(),get用@Query();get请求，参数可以直接定为指定类型，也可以引dto，但post要用dto
-  //一般get也用dto，因为dto可以让用户传参，直接指定类型的方式，参数一般是内部定死的
-  getHello(@Query() argsInfo: string): string {
-    argsInfo = 'MoMo';
-    return this.appService.getHello(argsInfo);
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(argsInfo: string): string {
-    return 'Hello World! Hello ' + argsInfo;
+  getHello(): string {
+    return 'Hello World!';
   }
 }
